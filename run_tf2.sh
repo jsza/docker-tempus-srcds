812f6ba9b63c764c58696511d2515449acf8447e
jayess <jayess@jayess.za.net>
2015-07-15 15:30:07 +0200

    Fixes.

--- run_tf2.sh
+++ run_tf2.sh
@@ -1,3 +1,3 @@
-$HOME/steamcmd/steamcmd.sh +runscript update_tf2.txt
-cd $HOME/steamcmd/tf2
-./srcds_run -game tf -steam_dir ~/steamcmd $@
+#!/bin/bash
+cd ~/steamcmd
+./steamcmd.sh +runscript update_tf2.txt
