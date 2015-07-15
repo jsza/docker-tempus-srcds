e3c91dfc4b0102cfd70bbe26d8b8c1f0c999a3b7
jayess <jayess@jayess.za.net>
2015-07-15 22:10:05 +0200

    Modify goh commandline.

--- run_tf2.sh
+++ run_tf2.sh
@@ -2,5 +2,5 @@
 cd ~/steamcmd
 ./steamcmd.sh +runscript update_tf2.txt
 cd /srv/srcds
-goh . -fi -sc metamod sourcemod stripper tf2items accelerator steamtools
+goh -afi -sc ./tf metamod sourcemod stripper tf2items accelerator steamtools
 exec ./srcds_run -game tf -steam_dir ~/steamcmd $@
