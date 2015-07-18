92bb5dcc3693658c99e1fb9668142cc5f2df7aa3
jayess <jayess@jayess.za.net>
2015-07-19 00:00:47 +0200

    Add disable teleporters custom script.

--- run_tf2.sh
+++ run_tf2.sh
@@ -4,8 +4,9 @@ SERVER_DIR="/srv/srcds"
 ADDONS_DIR="$SERVER_DIR/tf/addons"
 SM_PLUGINS_DIR="$ADDONS_DIR/sourcemod/plugins"
 TEMPUS_SM_PLUGINS_DIR="$SM_PLUGINS_DIR/tempus-sourcemod-plugins"
-CUSTOM_DIR="$SERVER_DIR/tf/custom/tempus"
-MAPS_DIR="$CUSTOM_DIR/maps"
+CUSTOM_DIR = "$SERVER_DIR/tf/custom"
+TEMPUS_CUSTOM_DIR="$CUSTOM_DIR/tempus"
+MAPS_DIR="$TEMPUS_CUSTOM_DIR/maps"
 
 cd ~/steamcmd
 ./steamcmd.sh +runscript update_tf2.txt
@@ -21,7 +22,16 @@ cd $MAPS_DIR
 
 if [ ! -d $TEMPUS_SM_PLUGINS_DIR ]; then
     mkdir $TEMPUS_SM_PLUGINS_DIR
-    git clone https://jsza@bitbucket.org/jsza/tempus-sourcemod-plugins.git $TEMPUS_SM_PLUGINS_DIR
+    git clone https://bitbucket.org/jsza/tempus-sourcemod-plugins.git $TEMPUS_SM_PLUGINS_DIR
+fi
+
+if [ ! -d "$CUSTOM_DIR/tf_disable_teleporters" ]
+then
+    mkdir "$CUSTOM_DIR/tf_disable_teleporters"
+    git clone https://bitbucket.org/tempusinc/tf_disable_teleporters.git "$CUSTOM_DIR/tf_disable_teleporters"
+# else
+#     cd "$CUSTOM_DIR/tf_disable_teleporters"
+#     git pull
 fi
 
 cd $TEMPUS_SM_PLUGINS_DIR
